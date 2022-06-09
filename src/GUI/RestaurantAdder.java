6cd1124c6de1c9a2a0472b51a332f8a0d4d91874
simminseok <100927702+simminseok@users.noreply.github.com>
2022-06-09 13:24:12 +0900

    Update RestaurantAdder.java

--- src/GUI/RestaurantAdder.java
+++ src/GUI/RestaurantAdder.java
@@ -7,43 +7,59 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
-public class RestaurantAdder extends JPanel {
-	
-	WindowFrame frame;
-	
-	public RestaurantAdder(WindowFrame frame) {
-	    this.frame = frame;
-		
-	    JPanel panel = new JPanel();
-	    panel.setLayout(new SpringLayout());
+import listeners.RestaurantAdderCancelListener;
+import listeners.RestaurantAdderListener;
+import manager.RestaurantManager;
 
-	    JLabel labelFavoliteranking = new JLabel("Favoliteranking: ", JLabel.TRAILING);
-	    JTextField fieldFavoliteranking = new JTextField(10);
-            labelFavoliteranking.setLabelFor(fieldFavoliteranking);
-            panel.add(labelFavoliteranking);
-            panel.add(fieldFavoliteranking);
-        
-            JLabel labelName = new JLabel("Name: ", JLabel.TRAILING);
-	    JTextField fieldName = new JTextField(10);
-            labelName.setLabelFor(fieldName);
-            panel.add(labelName);
-            panel.add(fieldName);
-        
-            JLabel labelAddress = new JLabel("Address: ", JLabel.TRAILING);
-	    JTextField fieldAddress = new JTextField(10);
-            labelAddress.setLabelFor(fieldAddress);
-            panel.add(labelAddress);
-            panel.add(fieldAddress);
-        
-            JLabel labelPhone = new JLabel("Phone: ", JLabel.TRAILING);
-	    JTextField fieldPhone = new JTextField(10);
-            labelPhone.setLabelFor(fieldPhone);
-            panel.add(labelPhone);
-            panel.add(fieldPhone);
-            panel.add(new JButton("save"));
-            panel.add(new JButton("cancel"));
-            SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6); 
-	    this.add(panel);
-	    this.setVisible(true);
+public class RestaurantAdder extends JPanel {
+
+	WindowFrame frame;
+	RestaurantManager restaurantmanager;
+
+	public RestaurantAdder(WindowFrame frame, RestaurantManager restaurantmanager) {
+		this.frame = frame;
+		this.restaurantmanager = restaurantmanager;
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+
+		JLabel labelFavoriteranking = new JLabel("Favoriteranking: ", JLabel.TRAILING);
+		JTextField fieldFavoriteranking = new JTextField(10);
+		labelFavoriteranking.setLabelFor(fieldFavoriteranking);
+		panel.add(labelFavoriteranking);
+		panel.add(fieldFavoriteranking);
+
+		JLabel labelName = new JLabel("Name: ", JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+
+		JLabel labelAddress = new JLabel("Address: ", JLabel.TRAILING);
+		JTextField fieldAddress = new JTextField(10);
+		labelAddress.setLabelFor(fieldAddress);
+		panel.add(labelAddress);
+		panel.add(fieldAddress);
+
+		JLabel labelPhone = new JLabel("Phone: ", JLabel.TRAILING);
+		JTextField fieldPhone = new JTextField(10);
+		labelPhone.setLabelFor(fieldPhone);
+
+		JButton saveButton = new JButton("save");
+		saveButton.addActionListener(new RestaurantAdderListener(fieldFavoriteranking, fieldName, fieldAddress, fieldPhone, restaurantmanager));
+
+		JButton cancelButton = new JButton("cancel");
+		cancelButton.addActionListener(new RestaurantAdderCancelListener(frame));
+
+		panel.add(labelPhone);
+		panel.add(fieldPhone);
+
+		panel.add(saveButton);
+		panel.add(cancelButton);
+
+		SpringUtilities.makeCompactGrid(panel, 5, 2, 6, 6, 6, 6); 
+
+		this.add(panel);
+		this.setVisible(true);
 	}
 }
