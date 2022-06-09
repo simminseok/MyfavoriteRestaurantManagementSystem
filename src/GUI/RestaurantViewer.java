8dda8dbf2a13b768929f1af0b5639031e50e3904
simminseok <100927702+simminseok@users.noreply.github.com>
2022-06-09 13:26:17 +0900

    Update RestaurantViewer.java

--- src/GUI/RestaurantViewer.java
+++ src/GUI/RestaurantViewer.java
@@ -12,35 +12,65 @@ import manager.RestaurantManager;
 import restaurant.RestaurantInput;
 
 public class RestaurantViewer extends JPanel {
-	
+
 	WindowFrame frame;
-	
+
 	RestaurantManager restaurantmanager;
+
+	public RestaurantManager getRestaurantmanager() {
+		return restaurantmanager;
+	}
+
+	public void setRestaurantmanager(RestaurantManager restaurantmanager) {
+		this.restaurantmanager = restaurantmanager;
+		this.removeAll();
+
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Favoriteranking");
+		model.addColumn("Name");
+		model.addColumn("Address");
+		model.addColumn("Contact Info.");
+
+		for (int i =0 ; i< restaurantmanager.size() ; i++) {
+			Vector row = new Vector();
+			RestaurantInput si = restaurantmanager.get(i);
+			row.add(si.getFavoriteranking());
+			row.add(si.getName());
+			row.add(si.getAddress());
+			row.add(si.getPhone());
+			model.addRow(row);
+		}
+
+		JTable table = new JTable(model);
+		JScrollPane sp = new JScrollPane(table);
+
+		this.add(sp);
+	}
 
 	public RestaurantViewer(WindowFrame frame, RestaurantManager restaurantmanager) {
 		this.frame = frame;
 		this.restaurantmanager = restaurantmanager;
 		System.out.println("***" + restaurantmanager.size() + "***");
-		
+
 		DefaultTableModel model = new DefaultTableModel();
-		model.addColumn("Favoliteranking");
+		model.addColumn("Favoriteranking");
 		model.addColumn("Name");
 		model.addColumn("Address");
 		model.addColumn("Contact Info.");
-		
+
 		for (int i =0 ; i< restaurantmanager.size() ; i++) {
-		    Vector row = new Vector();
-		    RestaurantInput si = restaurantmanager.get(i);
-		    row.add(si.getFavoliteranking());
-		    row.add(si.getName());
-		    row.add(si.getAddress());
-		    row.add(si.getPhone());
-		    model.addRow(row);
+			Vector row = new Vector();
+			RestaurantInput si = restaurantmanager.get(i);
+			row.add(si.getFavoriteranking());
+			row.add(si.getName());
+			row.add(si.getAddress());
+			row.add(si.getPhone());
+			model.addRow(row);
 		}
-		
+
 		JTable table = new JTable(model);
 		JScrollPane sp = new JScrollPane(table);
-		
+
 		this.add(sp);
 	}
 }
