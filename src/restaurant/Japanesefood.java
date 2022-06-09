6272595caaa69dc53b4b2ceb2768ef1837280ea7
simminseok <100927702+simminseok@users.noreply.github.com>
2022-06-09 13:38:49 +0900

    Update Japanesefood.java

--- src/restaurant/Japanesefood.java
+++ src/restaurant/Japanesefood.java
@@ -3,13 +3,13 @@ package restaurant;
 import java.util.Scanner;
 
 public class Japanesefood extends FusionRestaurant {
-
+	
 	public Japanesefood(RestaurantKind kind) {
 		super(kind);
 	}
 
 	public void getUserInput(Scanner input) {
-		setRestaurantfavoliteranking(input);
+		setRestaurantfavoriteranking(input);
 		setRestaurantName(input);
 		setRestaurantAddress(input);
 		setRestaurantPhonewithYN(input); 
